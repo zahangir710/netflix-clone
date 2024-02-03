50f91bd7e3a44391e7255da157cbcdafcb737508
zahangir710 <zahangir710@gmail.com>
2024-02-03 21:05:23 +0100

    add header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,37 @@
+import { useEffect, useState } from "react";
 import "./Header.css";
+
 function Header() {
-  return <div>this is header</div>;
+  const [show, setShow] = useState(false);
+  const transitionHeader = () => {
+    if (window.scrollY > 150) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionHeader);
+    return () => window.removeEventListener("scroll", transitionHeader);
+  }, []);
+
+  return (
+    <nav className={`header ${show && "header-black"}`}>
+      <div className="header-content">
+        <img
+          className="header-logo"
+          src="https://cdn.freebiesupply.com/logos/large/2x/netflix-2-logo-png-transparent.png"
+          alt="logo"
+        />
+        <img
+          className="header-avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt="avator"
+        />
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
