3acedb7dd21ed32aaef430060fc1eca5617e6a82
zahangir710 <zahangir710@gmail.com>
2024-02-06 09:22:08 +0100

    add user logic for header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,8 +3,9 @@ import "./Header.css";
 
 function Header() {
   const [show, setShow] = useState(false);
+  const user = null;
   const transitionHeader = () => {
-    if (window.scrollY > 150) {
+    if (window.scrollY > 150 && user) {
       setShow(true);
     } else {
       setShow(false);
@@ -17,18 +18,22 @@ function Header() {
   }, []);
 
   return (
-    <nav className={`header ${show && "header-black"}`}>
+    <nav className={`header ${show && user && "header-black"}`}>
       <div className="header-content">
         <img
           className="header-logo"
           src="https://cdn.freebiesupply.com/logos/large/2x/netflix-2-logo-png-transparent.png"
           alt="logo"
         />
-        <img
-          className="header-avatar"
-          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
-          alt="avator"
-        />
+        {!user ? (
+          <button>Sign in</button>
+        ) : (
+          <img
+            className="header-avatar"
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+            alt="avator"
+          />
+        )}
       </div>
     </nav>
   );
